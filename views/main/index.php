7b702a7f11f201c3b9aae946c5088f69c6808072
Meni Nuriel <nurielmeni@gmail.com>
2021-12-05 14:44:46 +0200

    Add wrapper

--- views/main/index.php
+++ views/main/index.php
@@ -2,7 +2,10 @@
 
 /* @var $model array */
 ?>
+<div class="app_params_settings-main-index">
 
-<?php foreach($model as $name => $file):?>
-    <p><a href="<?=Yii::$app->urlManager->createUrl(['/params/main/edit', 'file' => $file])?>"><?=$name?></a> </p>
-<?php endforeach;?>
+    <?php foreach($model as $name => $file):?>
+        <p><a href="<?=Yii::$app->urlManager->createUrl(['/params/main/edit', 'file' => $file])?>"><?=$name?></a> </p>
+    <?php endforeach;?>
+    
+</div>
